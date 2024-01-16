88d250dc56b40d0a7976f524869bcfd5f394a34b
MASKUN2 <maskunflower2@naver.com>
2024-01-16 22:22:20 +0900

    달력서비스 수정
    스트림API

--- src/main/java/com/maskun/projectdiary/service/CalendarService.java
+++ src/main/java/com/maskun/projectdiary/service/CalendarService.java
@@ -14,9 +14,8 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.temporal.TemporalAdjusters;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Slf4j
@@ -32,22 +31,29 @@ public class CalendarService {
      */
     public CalendarVo getCalendar(YearMonth yearMonth) {
         // 데이트 리스트 만들기
-        List<DateVo> dateVoList = getMonthLocalDateList(yearMonth).stream().map(DateVo::new).toList();
+        //List<DateVo> dateVoList = getMonthLocalDateList(yearMonth).stream().map(DateVo::new).toList();
+        LinkedHashMap<String, DateVo> dateVoMap = getMonthLocalDateList(yearMonth)
+                .stream()
+                .collect(Collectors.toMap(LocalDate::toString,
+                        DateVo::new,
+                        (exist, replacement)-> replacement,
+                        LinkedHashMap::new)
+                );
 
         // 해당월의 휴일찾고 매핑하기
-        getHolidayVoListOpt(yearMonth).orElse(new ArrayList<>()).forEach(holidayVo ->{
-            dateVoList.stream()
-                    .filter(dateVo -> dateVo.getDate().equals(holidayVo.getLocdate()))
-                    .findFirst()
-                    .ifPresent(dateVo -> dateVo.setHoliday(holidayVo.getDateName(), true));
-        });
-        getMonthMemoList(yearMonth).forEach(memo -> {
-            dateVoList.stream().filter(dateVo -> dateVo.getDate().equals(memo.getMemoDate()))
-                    .findFirst()
-                    .ifPresent(dateVo -> );
+        getHolidayVoListOpt(yearMonth)
+                .orElse(new ArrayList<>())
+                .forEach(holiday -> {
+                    Optional.ofNullable(dateVoMap.get(holiday.getLocdate().toString()))
+                            .ifPresent(dateVo -> dateVo.setHoliday(holiday.getDateName()));
+                });
+        //메모를 매핑하기
+        getMonthMemoList(yearMonth).forEach(memo->{
+            Optional.ofNullable(dateVoMap.get(memo.getMemoDate().toString()))
+                    .ifPresent(dateVo -> dateVo.addMemoOne(memo));
         });
 
-        return new CalendarVo(null, yearMonth, dateVoList);
+        return new CalendarVo(null, yearMonth, dateVoMap.values().stream().toList());
     }
     private List<Memo> getMonthMemoList(YearMonth yearMonth){
 
@@ -57,6 +63,7 @@ public class CalendarService {
         LocalDate startDate = yearMonth.atDay(1).with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
         LocalDate endDate = yearMonth.atEndOfMonth().with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
         return IntStream.iterate(0, n -> n + 1).mapToObj(startDate::plusDays).takeWhile(date -> date.isBefore(endDate)).toList();
+
     }
 
     private Optional<List<HolidayApiVo>> getHolidayVoListOpt(YearMonth yearMonth){
--- src/main/java/com/maskun/projectdiary/web/dto/DateVo.java
+++ src/main/java/com/maskun/projectdiary/web/dto/DateVo.java
@@ -1,10 +1,15 @@
 package com.maskun.projectdiary.web.dto;
 
+import com.maskun.projectdiary.domain.memo.Memo;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 @ToString
 @RequiredArgsConstructor
 @Getter
@@ -12,9 +17,18 @@ public class DateVo{
     private final LocalDate date;
     private String dateName;
     private Boolean isHoliday;
+    private List<Memo> memoList;
 
-    public void setHoliday(final String dateName, final boolean isHoliday){
+    public void setHoliday(final String dateName){
         this.dateName = dateName;
-        this.isHoliday = isHoliday;
+        this.isHoliday = true;
+    }
+
+    public void addMemoOne(Memo memo){
+        Objects.requireNonNull(memo);
+        if(this.memoList == null){
+            this.memoList = new ArrayList<>();
+        }
+        this.memoList.add(memo);
     }
 }
